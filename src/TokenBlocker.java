d5b0eac7955f40fc46a4fe2bca8f3801e7488c75
Nuubles <nuubles@users.noreply.github.com>
2021-02-16 19:19:40 +0200

    Clean code

--- src/TokenBlocker.java
+++ src/TokenBlocker.java
@@ -23,37 +23,61 @@ public class TokenBlocker extends Blocker {
 	public void block(Dataset set1, Dataset set2) {
 		for(int i = 0; i < set1.size(); ++i) {
 			String[] firstEntity = set1.getEntity(i);
-
 			for(int j = 0; j < set2.size(); ++j) {
 				String[] secondEntity = set2.getEntity(j);
+				compareAndStoreEntities(firstEntity, i, secondEntity, j);
+			}
+		}
+	}
 
-				// compare the two strings on how similar they are
-				for(int m = 0; m < firstEntity.length; ++m) {
-					for(int n = 0; n < secondEntity.length; ++n) {
 
-						// if similarity score is over 0.65, we determine the entities to be referring to the same entity
-						String[] intersection = intersection(firstEntity[m].toLowerCase(), secondEntity[n].toLowerCase());
-						for(String match : intersection) {
-							// if a match is found, add the intersections of the strings to entity clusters
-							Pair p1 = new Pair(0, i);
-							Pair p2 = new Pair(1, j);
+	/**
+	 * Compares two entities between each other, and if any words between them
+	 * match with each other, the entities will be added to a cluster with the name
+	 * of the matched word
+	 * @param firstEntity
+	 * @param firstEntityIndex
+	 * @param secondEntity
+	 * @param secondEntityIndex
+	 */
+	private void compareAndStoreEntities(String[] firstEntity, int firstEntityIndex, String[] secondEntity, int secondEntityIndex) {
+		// compare the two strings on how similar they are
+		for(int m = 0; m < firstEntity.length; ++m) {
+			for(int n = 0; n < secondEntity.length; ++n) {
 
-							if(entityClusters.containsKey(match)) {
-								List<Pair> entities = entityClusters.get(match);
-								if(!entities.contains(p1))
-									entities.add(p1);
-								if(!entities.contains(p2))
-									entities.add(p2);
-							} else {
-								List<Pair> entities = new ArrayList<Pair>();
-								entities.add(p1);
-								entities.add(p2);
-								entityClusters.put(match, entities);
-							}
-						}
-					}
+				String[] intersection = intersection(firstEntity[m].toLowerCase(), secondEntity[n].toLowerCase());
+
+				for(String match : intersection) {
+					// if a match is found, add the intersections of the strings to entity clusters
+					storeMatchesToCluster(match, firstEntityIndex, secondEntityIndex);
 				}
 			}
+		}
+	}
+
+
+	/**
+	 * Saves the given entities to a cluster. If no cluster exists for the given match,
+	 * a new one will be created
+	 * @param match name of cluster
+	 * @param firstEntityIndex index (row) for the entity in the first collection
+	 * @param secondEntityIndex index (row) for the entity in the second collection
+	 */
+	private void storeMatchesToCluster(String match, int firstEntityIndex, int secondEntityIndex) {
+		Pair p1 = new Pair(0, firstEntityIndex);
+		Pair p2 = new Pair(1, secondEntityIndex);
+
+		if(entityClusters.containsKey(match)) {
+			List<Pair> entities = entityClusters.get(match);
+			if(!entities.contains(p1))
+				entities.add(p1);
+			if(!entities.contains(p2))
+				entities.add(p2);
+		} else {
+			List<Pair> entities = new ArrayList<Pair>();
+			entities.add(p1);
+			entities.add(p2);
+			entityClusters.put(match, entities);
 		}
 	}
 
